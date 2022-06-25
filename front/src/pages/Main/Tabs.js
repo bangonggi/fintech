ccd5e0a19048f45628825ae46a6b1144389c9617
Jiwoo An <jiyouan9@gmail.com>
2022-06-25 21:01:48 +0900

    feat: 백엔드 연결

--- front/src/pages/Main/Tabs.js
+++ front/src/pages/Main/Tabs.js
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import ProgressBar from "@ramonak/react-progress-bar";
+import { useDispatch, useSelector } from "react-redux";
+import * as Api from "api";
 
 const tabs = [
   { query: "HP", title: "HP" },
@@ -9,15 +11,41 @@ const tabs = [
 ];
 
 const Tabs = () => {
+  const dispatch = useDispatch();
+  const [hp, setHp] = useState();
+  const [ep, setEp] = useState();
+  const [fp, setFp] = useState();
+
+  const getNoticeList = async () => {
+    try {
+      const res = await Api.get("users", "9140fef8-fa69-4547-98cc-122f442c7dd5");
+      setHp(res.data.hp);
+      setEp(res.data.ep);
+      setFp(res.data.fp);
+    } catch (e) {
+      // 에러처리
+    }
+  };
+ 
+  useEffect(() => {
+    getNoticeList();
+  }, []);
+
   return (
     <div>
       <TabsContainer>
-        {tabs.map((tab) => (
-          <Tab>
-            <div>{tab.title}</div>
-            <ProgressBar completed={60} style={{ width: "50%", }} />
-          </Tab>
-        ))}
+        <Tab>
+          <div>HP</div>
+          <ProgressBar completed={hp} style={{ width: "50%", }} />
+        </Tab>
+        <Tab>
+          <div>EP</div>
+          <ProgressBar completed={ep} style={{ width: "50%", }} />
+        </Tab> <Tab>
+          <div>FP</div>
+          
+          <ProgressBar completed={fp} style={{ width: "50%", }} />
+        </Tab>
       </TabsContainer>
     </div>
   );
--- front/src/pages/Main/TopBar.js
+++ front/src/pages/Main/TopBar.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 
 import CategoryButton from "../../components/CategoryButton";
+import * as Api from "api";
 
 const TopBar = ({ setIsOpenSideBar }) => {
+  const [name, setName] = useState();
   const handleCategoryBtnClick = () => {
     setIsOpenSideBar(true);
   };
 
+  
+  const [noticeList, setNoticeList] = useState([]);
+
+  const dispatch = useDispatch();
+
+  const getNoticeList = async () => {
+    try {
+      const res = await Api.get("users", "9140fef8-fa69-4547-98cc-122f442c7dd5");
+      setNoticeList(res.data.payload || []);
+      setName(res.data.charName);
+      console.log("res ===>", res);
+    } catch (e) {
+      // 에러처리
+    }
+  };
+
+  useEffect(() => {
+    getNoticeList();
+  }, []);
+
   return (
     <Container>
       <CategoryButton handleClick={handleCategoryBtnClick} />
-
+      <Title style={{ fontSize: "30px", fontFamily: "TmoneyRoundWindExtraBold" }}>{ name }의 집</Title>
     </Container>
   );
 };
@@ -33,4 +57,15 @@ const Container = styled.div`
   align-items: center;
   justify-content: left;
   color: white;
+`;
+
+const Title = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: "TmoneyRoundWindExtraBold";
+  font-size: 1.24rem;
+  position: relative;
 `;
